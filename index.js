1ec2207651c9f74ccc30f18f53f6057efa3bc93a
tarun-git-123 <tarunofficial303@gmail.com>
2024-03-01 20:10:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+app.get("/twitter", (req,res)=>{
+    res.send("<h1>Hello Twitter</h1>")
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
